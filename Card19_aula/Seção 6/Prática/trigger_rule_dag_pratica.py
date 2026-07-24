d7d01b18800e5eeb843e4c7fb703338658f0ac59
MaryaYsabella <maryaysabellacolatino17@gmail.com>
2026-07-24 09:39:10 -0300

    Update and rename Card19_aula/trigger_rule_dag_pratica.py to Card19_aula/Seção 6/Prática/trigger_rule_dag_pratica.py

--- Card19_aula/Seção 6/Prática/trigger_rule_dag_pratica.py
+++ Card19_aula/Seção 6/Prática/trigger_rule_dag_pratica.py
@@ -3,33 +3,32 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 from datetime import datetime
 
-# Pipeline de geração de relatório com diferentes trigger_rules
-
+# Pipeline de geração de relatório 
 default_args = {
-    'owner': 'Ronny_Pratica',
-    'start_date': datetime(2026, 1, 1),
+    'owner': 'Marya',
+    'start_date': datetime(2019, 1, 1),
 }
 
 def coletar_dados_crm():
-    print("Coletando dados do CRM...")
+    print("Coletando dados CRM")
 
 def coletar_dados_erp():
-    print("Coletando dados do ERP...")
+    print("Coletando dados ERP")
 
 def tratar_falha_coleta():
-    print("ALERTA: Pelo menos uma fonte de dados falhou! Registrando ocorrência.")
+    print("ALERTA: Fonte de dados falha!")
 
 def consolidar_dados():
-    print("Nenhuma fonte falhou — consolidando todos os dados para o relatório.")
+    print("Nenhuma fonte falhou! Dando continuidade ao relatório")
 
 def gerar_relatorio():
-    print("Gerando relatório final independente do resultado das fontes...")
+    print("Gerando relatório final independente do resultado das fontes")
 
 def notificar_sucesso():
-    print("Relatório gerado com sucesso! Enviando para os gestores.")
+    print("Relatório gerado com sucesso!")
 
 def notificar_falha():
-    print("Relatório gerado com pendências. Time de dados foi notificado.")
+    print("Relatório gerado com pendências!")
 
 with DAG(
     dag_id='trigger_rule_pratica_dag',
@@ -43,7 +42,7 @@ with DAG(
     coleta_crm = PythonOperator(
         task_id='coletar_dados_crm',
         python_callable=coletar_dados_crm,
-        trigger_rule='all_success'  # padrão só roda se upstream ok
+        trigger_rule='all_success' 
     )
 
     coleta_erp = PythonOperator(
@@ -52,21 +51,18 @@ with DAG(
         trigger_rule='all_success'
     )
 
-    # roda se pelo menos uma coleta 
     alerta_falha = PythonOperator(
         task_id='tratar_falha_coleta',
         python_callable=tratar_falha_coleta,
         trigger_rule='one_failed'
     )
 
-    #roda apenas se nenhuma coleta
     consolidar = PythonOperator(
         task_id='consolidar_dados',
         python_callable=consolidar_dados,
         trigger_rule='none_failed'
     )
 
-    #roda sempre independente do resultado das coletas
     gerar = PythonOperator(
         task_id='gerar_relatorio',
         python_callable=gerar_relatorio,

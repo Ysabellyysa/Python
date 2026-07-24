586d084a09e7acb9235490162c0db46d462b88b5
MaryaYsabella <maryaysabellacolatino17@gmail.com>
2026-07-24 09:22:51 -0300

    Update and rename Card19_aula/Parallel_Dag_Pratica.py to Card19_aula/Seção 5/Prática/Parallel_Dag_Pratica.py

--- Card19_aula/Seção 5/Prática/Parallel_Dag_Pratica.py
+++ Card19_aula/Seção 5/Prática/Parallel_Dag_Pratica.py
@@ -3,20 +3,21 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 #onfigurações básicas da DAG
+#
 default_args = {
-    'start_date': datetime(2026, 1, 1),
-    'owner': 'Ronny_Pratica',
+    'start_date': datetime(2019, 1, 1),
+    'owner': 'Marya',
 }
 
 def tarefa_paralela(id_task):
-    print(f"Executando processo paralelo número: {id_task}")
+    print(f"Executando processo paralelo: {id_task}")
 
 def finalizar():
-    print("Concluído: Todas as frentes foram processadas!")
+    print("Todas as frentes foram concluidas!")
 
 with DAG(dag_id='parallel_pratica_dag', schedule=None, default_args=default_args, catchup=False) as dag:
 
-    #Cria 3 tarefas que rodam ao mesmo tempo usando Python
+    #Cria 3 tarefas 
     processos = [
         PythonOperator(
             task_id=f'processo_paralelo_{i}',
@@ -25,7 +26,7 @@ with DAG(dag_id='parallel_pratica_dag', schedule=None, default_args=default_args
         ) for i in range(1, 4)
     ]
 
-    #aguarda os processos acima
+    #aguarda os processos 
     conclusao = PythonOperator(
         task_id='conclusao_geral',
         python_callable=finalizar

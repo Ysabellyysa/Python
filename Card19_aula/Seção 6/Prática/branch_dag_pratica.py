bec4d356f92a72bbe749d007b790e7b0e12c2c9e
MaryaYsabella <maryaysabellacolatino17@gmail.com>
2026-08-02 17:16:02 -0300

    Update branch_dag_pratica.py

--- Card19_aula/Seção 6/Prática/branch_dag_pratica.py
+++ Card19_aula/Seção 6/Prática/branch_dag_pratica.py
@@ -1,18 +1,18 @@
+#Bibliotecas utilizadas
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import BranchPythonOperator, PythonOperator
+from airflow.operators.python import BranchPythonOperator, PythonOperator  # branch decide qual caminho seguir
 from datetime import datetime
 
-# Branch DAG
 default_args = {
     'owner': 'Marya',
     'start_date': datetime(2019, 1, 1),
 }
 
 def verificar_tipo_pedido(**context):
-    valor = 150  
+    valor = 150  # valor fixo 
     if valor < 100:
-        return 'pedido_pequeno'
+        return 'pedido_pequeno' # retorna o task_id que deve rodar em seguida
     elif valor < 500:
         return 'pedido_medio'
     else:
@@ -27,34 +27,34 @@ def processar_medio():
 def processar_grande():
     print("Pedido grande: envio prioritário")
 
-def finalizar_pedido():
+def finalizar_pedido():  # função definida 
     print("Pedido registrado e confirmação enviado ao cliente!")
 
 with DAG(
     dag_id='branch_pratica_dag',
     default_args=default_args,
-    schedule=None,  
+    schedule=None,
     catchup=False,
     tags=['aula', 'branch']
 ) as dag:
 
-    inicio = EmptyOperator(task_id='inicio')  # EmptyOperator substitui DummyOperator 
+    inicio = EmptyOperator(task_id='inicio')
 
     branch = BranchPythonOperator(
         task_id='verificar_tipo_pedido',
-        python_callable=verificar_tipo_pedido
+        python_callable=verificar_tipo_pedido # o retorno da função decide qual task_id segue
     )
 
     pequeno = PythonOperator(task_id='pedido_pequeno', python_callable=processar_pequeno)
     medio   = PythonOperator(task_id='pedido_medio',   python_callable=processar_medio)
     grande  = PythonOperator(task_id='pedido_grande',  python_callable=processar_grande)
-    
-    # garantir q o end roda
+
     fim = EmptyOperator(
         task_id='fim',
         trigger_rule='none_failed_min_one_success'
+        # roda mesmo com os outros 2 caminhos skipped, desde que pelo menos 1 tenha sucesso
     )
 
     inicio >> branch
-    branch >> [pequeno, medio, grande]
-    [pequeno, medio, grande] >> fim
+    branch >> [pequeno, medio, grande] # branch aponta pra todas, mas só uma de fato executa
+    [pequeno, medio, grande] >> fim  # todas convergem pro fim

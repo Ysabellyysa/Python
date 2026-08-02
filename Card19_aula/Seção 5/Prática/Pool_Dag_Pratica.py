19e4bea93c738a6ddcf8ff732748804d6f2eb0cd
MaryaYsabella <maryaysabellacolatino17@gmail.com>
2026-08-02 17:18:04 -0300

    Add comments to Pool_Dag_Pratica.py
    Added comments to clarify the purpose of the DAG and tasks.

--- Card19_aula/Seção 5/Prática/Pool_Dag_Pratica.py
+++ Card19_aula/Seção 5/Prática/Pool_Dag_Pratica.py
@@ -1,7 +1,9 @@
+#Bibliotecas utilizadas 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
+#dono da DAG, data a partir da qual a DAG roda
 default_args = {
     'start_date': datetime(2019, 1, 1),
     'owner': 'Marya',
@@ -9,9 +11,8 @@ default_args = {
 
 with DAG(dag_id='pool_pratica_dag', schedule='@daily', default_args=default_args, catchup=False) as dag:
 
-    t1 = BashOperator(task_id='carga_01', bash_command='sleep 5', pool='default_pool')
+    t1 = BashOperator(task_id='carga_01', bash_command='sleep 5', pool='default_pool')  # limita concorrência via pool
     t2 = BashOperator(task_id='carga_02', bash_command='sleep 5', pool='default_pool')
     t3 = BashOperator(task_id='carga_03', bash_command='sleep 5', pool='default_pool')
 
-    #execuções
-    [t1, t2, t3]
+    [t1, t2, t3]# apenas lista, sem dependência entre elas

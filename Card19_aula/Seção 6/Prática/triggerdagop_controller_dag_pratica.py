82bb3b2d736627d94a0cfa002576b1a8b12f7191
MaryaYsabella <maryaysabellacolatino17@gmail.com>
2026-08-02 17:07:29 -0300

    Enhance triggerdagop_controller_dag_pratica.py with comments
    Added comments to clarify the purpose of each section and task in the DAG.

--- Card19_aula/Seção 6/Prática/triggerdagop_controller_dag_pratica.py
+++ Card19_aula/Seção 6/Prática/triggerdagop_controller_dag_pratica.py
@@ -1,5 +1,6 @@
+#Bibliotecas adicionadas 
 from airflow import DAG
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator  # operator que dispara outra DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime
@@ -13,9 +14,9 @@ def preparar_dados():
     print("Dados do pedido validados!")
 
 with DAG(
-    dag_id='triggerdagop_pratica',
+    dag_id='triggerdagop_pratica',                       
     default_args=default_args,
-    schedule='@once',
+    schedule='@once',# roda uma única vez, quando ativada
     catchup=False,
     tags=['aula', 'trigger_dagrun']
 ) as dag:
@@ -27,15 +28,15 @@ with DAG(
 
     disparar = TriggerDagRunOperator(
         task_id='processamento_pedido',
-        trigger_dag_id='triggerdagop_pratica',  
-        conf={
+        trigger_dag_id='triggerdagop_pratica',           
+        conf={    # dicionário enviado como 'conf' ao DAG disparado
             'pedido_id': 'PED-2019-001',
             'valor': 150.00,
             'cliente': 'Marya'
         },
-        wait_for_completion=False  
+        wait_for_completion=False  # não espera a DAG disparada terminar para seguir
     )
 
-    fim = EmptyOperator(task_id='fim')
+    fim = EmptyOperator(task_id='fim') #marca o fim da pipeline
 
-    preparar >> disparar >> fim
+    preparar >> disparar >> fim # define a ordem de execução 
